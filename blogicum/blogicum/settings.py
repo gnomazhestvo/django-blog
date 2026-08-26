bf1f450a06c8b8aeee9f7962602e9cdef815b774
Lev <konstantinov.lev@outlook.com>
2026-08-26 13:09:10 +0000

    complete homework

--- blogicum/blogicum/settings.py
+++ blogicum/blogicum/settings.py
@@ -121,11 +121,11 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'static_dev'
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-STATICFILES_DIRS = [
-    BASE_DIR / 'static_dev'
-]
